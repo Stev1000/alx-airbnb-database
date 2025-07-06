784ccd4b94492b458b71ad7e31cb4c6cd832f69e
Stevo <nsanzestevo1@gmail.com>
2025-07-06 19:01:28 +0200

    Added EXPLAIN ANALYZE commands (commented out) to database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,3 +6,8 @@ CREATE INDEX idx_booking_dates ON Booking (start_date, end_date);
 
 -- Review table index
 CREATE INDEX idx_review_rating ON Review (rating);
+
+-- Example EXPLAIN ANALYZE commands (for testing purposes):
+-- EXPLAIN ANALYZE SELECT * FROM Property WHERE location = 'New York, NY';
+-- EXPLAIN ANALYZE SELECT * FROM Booking WHERE start_date >= '2024-01-15' AND start_date <= '2024-01-31';
+-- EXPLAIN ANALYZE SELECT * FROM Review WHERE rating = 5;
