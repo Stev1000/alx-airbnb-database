7beb3fb3abe2c75f2c1a857eb634eedb69018107
Stevo <nsanzestevo1@gmail.com>
2025-07-06 16:57:53 +0200

    joins_queries.sql and README.md file are added in the repository

---
+++ database-adv-script/README.md
@@ -0,0 +1,27 @@
+# Complex SQL Joins
+
+This task focuses on writing complex SQL queries using different types of JOIN operations to explore relationships between tables.
+
+## Queries
+
+### 1. INNER JOIN
+
+Retrieves all bookings along with the users who made them.
+
+### 2. LEFT JOIN
+
+Retrieves all properties and their associated reviews, including those with no reviews.
+
+### 3. FULL OUTER JOIN
+
+Retrieves all users and all bookings, even when a user hasn't made a booking or a booking isn't associated with a user.
+
+## Files
+
+- `joins_queries.sql` - Contains the SQL queries.
+- `README.md` - Describes the queries and their purpose.
+
+## Repository
+
+GitHub: [alx-airbnb-database](https://github.com/YOUR_USERNAME/alx-airbnb-database)  
+Directory: `database-adv-script`
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,39 @@
+
+-- 1. INNER JOIN: Get all bookings and their corresponding users
+SELECT
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM
+    bookings
+INNER JOIN
+    users ON bookings.user_id = users.id;
+
+-- 2. LEFT JOIN: Get all properties and their reviews (including those with no reviews)
+SELECT
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM
+    properties
+LEFT JOIN
+    reviews ON properties.id = reviews.property_id;
+
+-- 3. FULL OUTER JOIN: Get all users and all bookings, even unmatched
+SELECT
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM
+    users
+FULL OUTER JOIN
+    bookings ON users.id = bookings.user_id;
