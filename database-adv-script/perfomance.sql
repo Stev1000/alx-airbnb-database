4930ab009aacd0e48d617bcce35571fb57ca2bb7
Stevo <nsanzestevo1@gmail.com>
2025-07-06 19:45:47 +0200

    Corrected content of perfomance.sql and optimization_report.md

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -94,3 +94,31 @@ JOIN
     Property p ON ub.property_id = p.property_id
 JOIN
     Payment pm ON ub.booking_id = pm.booking_id;
+
+-- perfomance.sql
+
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pm.payment_id,
+    pm.amount,
+    pm.payment_date,
+    pm.payment_method
+FROM
+    Booking b
+JOIN
+    "User" u ON b.user_id = u.user_id
+JOIN
+    Property p ON b.property_id = p.property_id
+JOIN
+    Payment pm ON b.booking_id = pm.booking_id;
